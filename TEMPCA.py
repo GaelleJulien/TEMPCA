20e76f795838b5b1fc3e3d8ee1bcbda7cf3a82d9
Gaëlle <gaelle.julien@edu.ece.fr>
2023-04-21 15:28:40 +0200

    récup de l'excel

--- TEMPCA.py
+++ TEMPCA.py
@@ -1,1 +1,13 @@
 
+from openpyxl import Workbook
+from openpyxl import load_workbook
+
+
+workbook = load_workbook(filename = "donnees_actimetres.xlsx")
+sheet = workbook.active
+
+print(sheet["B3"].value)
+
+
+workbook.save(filename="test.xlsx")
+
