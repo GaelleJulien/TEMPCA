f868e538b93cd030680e82083431b922738200db
Gaëlle <gaelle.julien@edu.ece.fr>
2023-04-24 10:27:56 +0200

    début écriture

--- TEMPCA.py
+++ TEMPCA.py
@@ -5,10 +5,22 @@ from classes import Stats
 from mapping import USERID, ACTUAL_SLEEP
 
 
+stats = []
 
-workbook = load_workbook(filename = "donnees_actimetres.xlsx", read_only=True)
-for sheet in workbook : 
-    stats = []
+workbook2 = load_workbook(filename = "donnees_actimetres.xlsx", read_only=True)
+for sheet in workbook2 : 
     stat = Stats(id = sheet[USERID].value, actual_sleep_rate = sheet[ACTUAL_SLEEP].value)
+    
     stats.append(stat)
     print(stats)
+
+workbook = Workbook();
+sheet = workbook.active
+
+sheet.append(["UserID", "actual_sleep_rate"])
+for stat in stats : 
+    data = [stat.id, stat.actual_sleep_rate]
+    sheet.append(data)
+
+
+workbook.save(filename= "test.xlsx")
